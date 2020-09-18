a0c44f6a19fd950f5ba66fb24e872817de1dcd71
LennWi <lennart.willms@jade-hs.de>
2020-09-18 12:20:05 +0200

    Kommentare hinzugefügt

--- BubbleSort_HSP/Program.cs
+++ BubbleSort_HSP/Program.cs
@@ -9,13 +9,13 @@ namespace BubbleSort_HSP
 {
     class Program
     {
-        static void Main()
+        static void Main()  //Hauptprogramm
         { 
-            int[] intArray = new int[10] { 7, 6, 3, 2, 1, 6, 23, 1, 78, 4};
+            int[] intArray = new int[10] { 7, 6, 3, 2, 1, 6, 23, 1, 78, 4};    //erzeuge Array und befülle es
 
-            Sortierer sort1 = new Sortierer(intArray);
+            Sortierer sort1 = new Sortierer(intArray);  //neues Objekt Sortierer, Array wird übergeben
 
-            Console.ReadKey();
+            Console.ReadKey();  //Warte auf Tastendruck
         }
     }
 
@@ -23,15 +23,15 @@ namespace BubbleSort_HSP
     {
 
         private bool istGetauscht;
-        private int[] inArray;
-        private int[] outArray;
+        private int[] inArray;          //eigehendes Array (unsortiert)
+        private int[] outArray;         //ausgehendes Array (sortiert)
 
         public Sortierer(int[] intArray)
         {
             inArray = intArray;
             outArray = BubbleSort(inArray);
 
-            foreach(int iZahl in outArray)
+            foreach(int iZahl in outArray)  //Ausgabe des sortierten Arrays
             {
                 Console.WriteLine(iZahl);
             }
@@ -47,6 +47,7 @@ namespace BubbleSort_HSP
                 {
                     if (intArray[i] > intArray[i + 1])
                     {
+                        // Zahlen werden vertauscht
                         int iTemp = intArray[i];
                         intArray[i] = intArray[i + 1];
                         intArray[i + 1] = iTemp;
